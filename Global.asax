b9ee92fbe03d4943c297689c012c19a68a1054e4
Economos <me140315@ohio.edu>
2020-01-26 20:27:04 -0500

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="me140315_MIS4200S.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(me140315_MIS4200S.Startup))]
+namespace me140315_MIS4200S
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
